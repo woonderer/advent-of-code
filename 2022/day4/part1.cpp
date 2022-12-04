c5a36d82e0a8615ac0c1ce0823c1857c3ccc6d80
woonder <77258100+woonderer@users.noreply.github.com>
2022-12-04 09:19:02 +0100

    Add files via upload

---
+++ 2022/day4/part1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    freopen("input.in", "r", stdin);
+
+    int ans = 0;
+    string line;
+    while (getline(cin, line))
+    {
+        pair <int, int> a, b;
+        sscanf(line.c_str(), "%d-%d,%d-%d", &a.first, &a.second, &b.first, &b.second);
+        if (a.first > b.first || (a.first == b.first && a.second < b.second)) swap(a, b);
+        if (a.first <= b.first && b.second <= a.second) ans++;
+    }
+    cout << ans;
+}
