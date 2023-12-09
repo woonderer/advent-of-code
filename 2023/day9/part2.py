563bda8ae3bc3ba3760519b6df1734606c7c638c
woonder <77258100+woonderer@users.noreply.github.com>
2023-12-09 09:32:15 +0100

    Add files via upload

---
+++ 2023/day9/part2.py
@@ -0,0 +1,8 @@
+import os
+with open(os.path.dirname(__file__) + "\\input.in") as file:
+    f = file.read().strip().split('\n')
+
+def solve(T):
+    return 0 if len(set(T)) == 1 and 0 in T else T[0] - solve([T[i] - T[i - 1] for i in range(1, len(T))])
+
+print(sum(solve(list(map(int, line.split()))) for line in f))
