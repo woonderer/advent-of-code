1339623066a80362670a3353eb616e4319b0644f
woonder <77258100+woonderer@users.noreply.github.com>
2023-12-11 06:38:56 +0100

    Add files via upload

---
+++ 2023/day11/part1.py
@@ -0,0 +1,32 @@
+import os
+with open(os.path.dirname(__file__) + "/input.in") as file:
+    f = file.read().strip().split('\n')
+
+row, col = [0] * len(f), [0] * len(f[0])
+for i, line in enumerate(f):
+    if i: row[i] = row[i - 1]
+    if '#' not in line: row[i] += 1
+
+for j in range(len(f[0])):
+    if j: col[j] = col[j - 1]
+    good = True
+    for i in range(len(f)):
+        if f[i][j] == '#':
+            good = False
+            break
+
+    if good: col[j] += 1
+
+pos = [] 
+for i in range(len(f)):
+    for j in range(len(f[0])):
+        if f[i][j] == '#':
+            pos.append((i, j))
+
+ans = 0
+for i in range(len(pos)):
+    for j in range(i + 1, len(pos)):
+        ans += abs(pos[i][0] - pos[j][0]) + abs(pos[i][1] - pos[j][1])
+        ans += col[max(pos[i][1], pos[j][1])] - col[min(pos[i][1], pos[j][1])]
+        ans += row[max(pos[i][0], pos[j][0])] - row[min(pos[i][0], pos[j][0])]
+print(ans)
